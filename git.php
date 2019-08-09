fe861ae1853c1816b620c61d00a0e0301d915e1c
Fahad <fahadbinmnr@gmail.com>
2019-08-09 15:47:55 +0600

    a function gitDemo added

--- git.php
+++ git.php
@@ -1,6 +1,27 @@
 <?php
 
-    echo "this is my php file";
-    echo "this is second file";
+    echo "this is my php file<br>";
+    echo "this is second file<br>";
+
+   
+    function gitDemo(&$name1,&$email1,&$id1) {
+
+         $name = "Fahad Bin Munir";
+         $email = "fahadbinmnr@gmail.com";
+         $id = 1226;
+
+        if ($name1 == $name && $email1 == $email && $id1 == $id) {
+            echo "Access Granted<br>";
+        } else {
+            echo "Access Denied<br>";
+        }
+        
+    }
+
+    $namecheck = "Fahad Bin Munir";
+    $emailcheck = "fahadbinmnr@gmail.com";
+    $idcheck = 1226;
+
+    gitDemo($namecheck,$emailcheck,$idcheck);
     
 ?>
